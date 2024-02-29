5361ccbc39b7a100f9ee10911c1d34889e16d864
Trần Văn Hậu <hautran192002@gmail.com>
2024-02-29 16:11:28 +0700

    lab5
    lab5

---
+++ Lab5-Lab8/src/Lab5/Lab5bai1.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package Lab5;
+
+/**
+ * 
+ */
+public class Lab5bai1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
